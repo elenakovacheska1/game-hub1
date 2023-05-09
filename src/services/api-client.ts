9cd9382b2183a87c6138abbd584856c2efd6bff1
Elena Kovacheska <kovacheskaelena@gmail.com>
2023-05-09 22:03:16 +0200

    updated api key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
 	baseURL: "https://api.rawg.io/api",
 	params: {
-		key: "b195822e54994f6b92b0c884d5b364f6",
+		key: "a467355e05e74f5e9a5c3424119bc1f6",
 	},
 });
