0553babb016e4f81d9f2781600d45b7ceb7d7b44
Elena Kovacheska <kovacheskaelena@gmail.com>
2023-05-10 17:05:04 +0200

    integrated error tracking with Sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import * as Sentry from "@sentry/react";
 import App from "./App.tsx";
 import ErrorPage from "./components/ErrorPage.tsx";
 import "./index.css";
@@ -8,6 +8,17 @@ import Game from "./components/Game.tsx";
 import SignUp from "./components/SignUp.tsx";
 import Login from "./components/Login.tsx";
 import { render } from "react-dom";
+
+Sentry.init({
+	dsn:
+		"https://fd9cb41f247a47a5a0d26d1126ea1018@o4504915258114049.ingest.sentry.io/4505160284766208",
+	integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+	// Performance Monitoring
+	tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+	// Session Replay
+	replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+	replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 const router = createBrowserRouter([
 	{
